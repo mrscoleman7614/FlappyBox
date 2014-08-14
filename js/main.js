7b11e49264c23cbe3d782f23f4059fb6db582421
kwashington335@gmail.com <>
2014-08-14 15:19:03 +0000

    initial java

--- js/main.js
+++ js/main.js
@@ -1,31 +1,51 @@
-//intialize Phaser Engine. Create a 400x490px game.
+//Initialize Phaser Engine. Create a 400x490px game!
 
-var game = new Phaser.Game(400,490,Phaser.AUTO,"gameDiv");
+var game = new Phaser.Game(400, 490, Phaser.AUTO, 'gameDiv');
 
-//Create our 'main' state that will contain the game.
-//This is the body of the game itself. It contains all relevant code.
+//Create our 'main' state that will contain the game
+//This is the body of a the game itself
+//It should contain all code related to the game itself
 
-var mainState = {
-
-  preload: function() {
-    //This function will execute at the beginning of the game
-    //Here we'll load all of our assets (art, music, etd)
+var mainState = { 
+  preload: function () {
+    //This function will execute at the beginning
+    //Which is where we'll load our assets for the game
     
+    //Set the background color of the game
+    game.stage.backgroundColor = "#71c5cf" ; 
+    
+    
+    game.load.image('bird', 'assets/bird.png');
+    
+    game.load.image('pipe', 'assets/pipe.png');
   },
   
   create: function() {
-    //The create function is called right after the preload f(x)
-    //This is where we'll set up the game assets from scratch
-  },
+    //This function is called right after preload function
+    //This is where we set up the game assets from earlier
+    
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+ 
+ 
+this.bird = this.game.add.sprite(100, 245,'bird');
+ 
+ 
+ //Now that we have a bird and gravity...we need to tell the bird
+ //to react to the gravity
+ 
+ game.physics.arcade.enable(this.bird);
+ 
+ this.bird.body.gravity.y=1000;
+ },
+ 
+  update: function () {
+  //This function runs 60 times per second
+  
+},
+  
+};
 
-  update: function() {
-    //This function is called 60 times a second
-    //This contains the games logic and all time related actions
-  }
 
-}
-
-
-//Add and start the gameState
+//Add and start the 'mainState to state the game
 game.state.add('main', mainState);
 game.state.start('main');
